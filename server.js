10a33855f6b1b578fe06d96e9cfbe54432cf1a6a
DESKTOP-H7UGBLT\geust <rlacksgh08@naver.com>
2023-04-03 15:05:58 +0900

    진행상황 커밋

--- server.js
+++ server.js
@@ -4,17 +4,34 @@ let tweets = [
     {
         id:"1",
         text:"hello",
+        userId: "2"
     },
     {
         id:"2",
         text:"every",
+        userId: "1"
+    }
+];
+
+let users = [
+    {
+        id: "1",
+        firstName: "Kim",
+        lastName: "ChanHo"
+    },
+    {
+        id: "2",
+        firstName: "haha",
+        lastName: "hohoho"
     }
 ];
 
 const typeDefs = gql`
     type User {
         id: ID!
-        username: String!
+        firstName: String!
+        lastName: String!
+        fullName: String!
     }
 
     type Tweet {
@@ -25,27 +42,63 @@ const typeDefs = gql`
 
     type Query {
         allTweets: [Tweet!]!
+        allUsers: [User!]!
+        allMovies: [Movie!]!
         tweet(id: ID): Tweet
-        ping: String!
+        movie(id: ID): Movie
     }
     
     type Mutation {
         postTweet(text:String!, userId: ID!): Tweet!
         deleteTweet(id: ID!): Boolean!
     }
+    
+    type Movie {
+        id: Int!
+        url: String!
+        imdb_code: String!
+        title: String!
+        title_english: String!
+        title_long: String!
+        slug: String!
+        year: Int!
+        rating: Float!
+        runtime: Float!
+        genres: [String]!
+        summary: String
+        description_full: String!
+        synopsis: String
+        yt_trailer_code: String!
+        language: String!
+        background_image: String!
+        background_image_original: String!
+        small_cover_image: String!
+        medium_cover_image: String!
+        large_cover_image: String!
+    }
 `;
 
 const resolvers = {
     Query: {
-        tweet(root, { id }) {
+        tweet(_, { id }) {
             return tweets.find(tweets => tweets.id === id);
         },
-        ping() {
-            console.log('call ping');
-            return "pong";
+        movie(_, { id }) {
+            return fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+                    .then((r) => r.json())
+                    .then((json) => json.data.movie);
         },
         allTweets() {
             return tweets;
+        },
+        allUsers() {
+            console.log('allUsers = ');
+            return users;
+        },
+        allMovies() {
+            return fetch("https://yts.mx/api/v2/list_movies.json")
+                    .then((r) => r.json())
+                    .then((json) => json.data.movies);
         }
     },
     Mutation: {
@@ -68,6 +121,19 @@ const resolvers = {
 
             tweets = tweets.filter((tweet) => tweet.id !== id);
             return true;
+        }
+    },
+    User: {
+        firstName({ firstName }) {
+            return `${firstName}`;
+        },
+        fullName({ firstName, lastName }) {
+            return `${firstName} ${lastName}`;
+        }
+    },
+    Tweet: {
+        author({userId}) {
+            return users.find(user => user.id === userId)
         }
     }
 }
